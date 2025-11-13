e34ee5acf03263401ba3fd18771ed7cbc7db65d7
luc-lieu <luc.lieu@edu.esiee.fr>
2025-11-13 10:56:01 +0000

    Rendu 04-primes

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
+"""Import sqrt(n) : used in isprime()."""
 from math import sqrt
 
 #### Fonction secondaire
 
 
-def isprime(p):
-
+def isprime(p: int) -> bool:
+    """Check if the integer valer is prime.
+    Args:
+        p (int): Integer to check.
+    Returns:
+        bool: Truth value of the statement "Argument is prime" .
+    """
     # votre code ici
 
-    pass
+    if p < 2:
+        return False
+
+    psup = int(sqrt(p)) + 1 # supremum of the test interval in range()
+
+    for d in range(2, psup):
+        if p % d == 0:
+            return False
+    return True
 
 #### Fonction principale
 
 
 def main():
-
+    """Execution for the 100 first integers."""
     # vos appels à la fonction secondaire ici
 
     for n in range(100):
